3cf1071f4e5500f7b28f5f70eba701e45213937f
Cristhian-Chavez <crchavez@tu.com.py>
2023-05-07 11:24:45 -0400

    connect to mongoose and create rest file

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require("http");
+const mongoose = require('mongoose');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
 app.use(cors());
 const server = http.createServer(app);
+
+mongoose.connect('mongodb://127.0.0.1:27017/retrospective-project', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+
+// models
+const {Column, Card} = require('./models/Model')
 
 
 const io = new Server( server, {
